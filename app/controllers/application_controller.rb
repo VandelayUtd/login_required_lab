5e7f8278e1840e8391eacc24333aa58f08843259
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-01 15:26:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user 
+    session[:name] ? current_user = session[:name] : nil
+    current_user
+  end
+
+  def homepage 
+    @user = current_user
+  end
+
+  private 
+
+  def require_login
+    return head(:forbidden) unless current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :secrets
+
+  resources :application
+
 end
